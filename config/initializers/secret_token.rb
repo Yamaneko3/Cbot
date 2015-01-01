ed8cd8a7694a2528f5e1fb5d97a2e0b22089cf36
takimoto-katsuki <katsuki.takimoto@dena.com>
2015-01-01 14:11:49 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cbot::Application.config.secret_token = '5721187269ba8b091e478febe51b0375cc476dfa5ebf0025bae573dff3e8f86a67b41a900cef2a99a69b31e2bde3420504607a04378324a2e846fa4be7bb4d6d'
